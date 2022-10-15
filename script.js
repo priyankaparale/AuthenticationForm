bc52dc9452b63b003e84da2062c84d1598641493
Priyanka <paralepriyanka8@gmail.com>
2022-10-15 17:27:53 +0530

    added html JS style

---
+++ script.js
@@ -0,0 +1,45 @@
+//access granted
+//access denied
+
+let userEmail, userPassord
+
+const signUP () => {
+    userEmail = document.getElementById('email').value
+    userPassword = document.getElementById('password').value
+
+    document.getElementById('')
+}
+
+const signIn () => {
+    let enteredEmail = document.getElementById('login-email').value
+    let enteredPassword = document.getElementById('login-password').value
+
+    if(enteredEmail === userEmail && enteredPassword === userEmail){
+        alert('Access granted')
+    }else{
+        alert('Access denied')
+    }
+}
+
+
+
+const goToHoe = () => {
+    document.getElementById('home').style.display = 'block';
+    document.getElementById('sign-in').style.display = 'none';
+    document.getElementById('sign-up').style.display = 'none';
+}
+
+const goToSignUp = () => {
+    document.getElementById('sign-up').style.display = 'block';
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('sign-in').style.display = 'none';
+}
+
+const goToSignIn = () => {
+    document.getElementById('sign-in').style.display = 'block';
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('sign-up').style.display = 'none';
+}
+
+//access granted
+//access denied
